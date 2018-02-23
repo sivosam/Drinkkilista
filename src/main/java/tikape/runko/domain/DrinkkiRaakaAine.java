f13f58389054981d9df68d0601cc6c1b3232b271
saaraemilia <saaraemilia@Saara-MacBook-Pro-6.local>
2018-02-23 15:31:39 +0200

    drinkkiraakaaine integereiksi

--- src/main/java/tikape/runko/domain/DrinkkiRaakaAine.java
+++ src/main/java/tikape/runko/domain/DrinkkiRaakaAine.java
@@ -2,13 +2,13 @@ package tikape.runko.domain;
 
 public class DrinkkiRaakaAine {
 
-    private int raaka_aine_id;
-    private int drinkki_id;
+    private Integer raaka_aine_id;
+    private Integer drinkki_id;
     private String jarjestys;
     private String maara;
     private String ohje;
 
-    public DrinkkiRaakaAine(int raaka_aine_id, int drinkki_id, String jarjestys, String maara, String ohje) {
+    public DrinkkiRaakaAine(Integer raaka_aine_id, Integer drinkki_id, String jarjestys, String maara, String ohje) {
         this.raaka_aine_id = raaka_aine_id;
         this.drinkki_id = drinkki_id;
         this.jarjestys = jarjestys;
@@ -16,11 +16,11 @@ public class DrinkkiRaakaAine {
         this.ohje = ohje;
     }
 
-    public int getRaakaAineId() {
+    public Integer getRaakaAineId() {
         return this.raaka_aine_id;
     }
 
-    public int getDrinkkiId() {
+    public Integer getDrinkkiId() {
         return this.drinkki_id;
     }
 
